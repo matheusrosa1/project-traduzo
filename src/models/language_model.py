9e38ad78b78cedbfc9f04a0e96dd57cabbd5b78e
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-24 15:11:25 -0300

    feat: LenguageModel -> init, to_dict

---
+++ src/models/language_model.py
@@ -0,0 +1,13 @@
+from typing import Dict, List
+from database.db import db
+from models.abstract_model import AbstractModel
+
+
+class LanguageModel(AbstractModel):
+    _collection = db["languages"]
+
+    def __init__(self, data: List[Dict]):
+        super().__init__(data)
+
+    def to_dict(self):
+        return {"name": self.data["name"], "acronym": self.data["acronym"]}
