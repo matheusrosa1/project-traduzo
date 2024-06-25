4d03cb55085086ae441c8f96e7033e564eb18c93
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-25 17:47:20 -0300

    feat: translate controller POST / index.html

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template
+from deep_translator import GoogleTranslator
+from flask import Blueprint, render_template, request
 from models.language_model import LanguageModel
 
 translate_controller = Blueprint("translate_controller", __name__)
 
 
-@translate_controller.route("/", methods=["GET"])
+@translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
 
@@ -12,6 +13,19 @@ def index():
     translate_from = "pt"
     translate_to = "en"
     translated = "What do you want to translate?"
+
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate", "")
+        translate_from = request.form.get("translate-from", "pt")
+        translate_to = request.form.get("translate-to", "en")
+
+        if text_to_translate:
+            try:
+                translated = GoogleTranslator(
+                    source=translate_from, target=translate_to
+                ).translate(text_to_translate)
+            except Exception as e:
+                translated = f"Erro na tradução: {str(e)}"
 
     return render_template(
         "index.html",
