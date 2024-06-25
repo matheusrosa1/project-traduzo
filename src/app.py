63d01308ed8b14c6da7b1982688926f18d1bf4f1
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-24 23:05:28 -0300

    featuring: translate_controller -> index.html incompleto

--- src/app.py
+++ src/app.py
@@ -5,12 +5,15 @@ from controllers.admin_controller import admin_controller
 from os import environ
 from waitress import serve
 
+from controllers.translate_controller import translate_controller
+
 
 app = Flask(__name__)
 app.template_folder = "views/templates"
 app.static_folder = "views/static"
 
 app.register_blueprint(admin_controller, url_prefix="/admin")
+app.register_blueprint(translate_controller, url_prefix="/")
 
 
 def start_server(host="0.0.0.0", port=8000):
