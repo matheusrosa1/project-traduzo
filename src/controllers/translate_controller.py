a63934f9b05695b52da8b5d8462b39e3681fe3ad
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-29 21:43:28 -0300

    req. 7 - traducao reversa

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -35,3 +35,22 @@ def index():
         translate_to=translate_to,
         translated=translated,
     )
+
+
+@translate_controller.route("/reverse", methods=["POST"])
+def reverse():
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    text_to_translate = request.form.get("text-to-translate")
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
