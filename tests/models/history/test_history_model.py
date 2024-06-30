89c95fec6f34c97e4242206d58e88feda41d5419
Matheus Rosa <matheusrosataxa@gmail.com>
2024-06-30 17:43:02 -0300

    feat: req. 8 tests historico traducoes

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,18 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+
+import pytest
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
+@pytest.mark.usefixtures("prepare_base")
 def test_request_history():
-    raise NotImplementedError
+    json_data = HistoryModel.list_as_json()
+    data = json.loads(json_data)
+
+    assert data  # Verifica se a lista não está vazia
+    assert len(data) == 2  # Verifica se a lista tem 2 elementos
+    assert data[0]["text_to_translate"] == "Hello, I like videogame"
+    assert data[1]["text_to_translate"] == "Do you love music?"
+    assert data[0]["translate_from"] == "en"
+    assert data[1]["translate_from"] == "en"
